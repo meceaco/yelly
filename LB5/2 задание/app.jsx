61537fb9930df725ce46fb5884ea72ea142d690c
Ilya Shiryaev <mioda6164@gmail.com>
2025-05-24 00:21:47 +0300

    Лабы

---
+++ LB5/2 задание/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customer = {
+	first_name: "TAKCUCT",
+	last_name: "AIIIOT"
+};
+
+//              Вставляем внуть <div> тега
+const output = <div></div>;
+
+ReactDOM.render(output, document.querySelector("#myDiv"));
